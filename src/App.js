d8d49e0c69aae1b878ed016c98917215a654b5b6
Brian White <brian@thirdember.com>
2024-05-02 16:52:40 -0400

    add a search field

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
 import axios from "axios";
 import { DataType, Table } from 'ka-table';
 import { useQuery } from "react-query";
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import "./App.css";
 
-const retrieveBooks = async (page, limit) => {
+const retrieveBooks = async (page, limit, search) => {
   const response = await axios.get(
-    `http://pihome.local:5384/books?page=${page}&limit=${limit}`,
+    `http://pihome.local:5384/books?page=${page}&limit=${limit}&search=${search}`,
   );
   return response.data;
 };
 
 const App = () => {
   const [paging, setPaging] = useState({ currentPage: 1, pageSize: 10 });
+  const [search, setSearch] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
 
   const {
     data: books,
     error,
     isLoading,
-  } = useQuery(["bookData", paging.currentPage, paging.pageSize],
+  } = useQuery(["bookData", paging.currentPage, paging.pageSize, search],
     ({ queryKey }) => {
-      const [, page, limit] = queryKey;
-      return retrieveBooks(page, limit);
+      const [, page, limit, search] = queryKey;
+      return retrieveBooks(page, limit, search);
     });
 
   if (isLoading) return <div>Fetching books...</div>;
   if (error) return <div>An error occurred: {error.message}</div>;
+
+  const onInputChange = (event) => {
+    setInputValue(event.target.value);
+  }
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    setSearch(inputValue);
+    setPaging({ currentPage: 1, pageSize: paging.pageSize });
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
 
   const onFirstPage = () => {
     setPaging(prev => ({ ...prev, currentPage: 1 }));
@@ -45,6 +68,10 @@ const App = () => {
 
   return (
     <div className='bookTable'>
+      <form onSubmit={onSearchSubmit}>
+        <input ref={inputRef} className="search-input" type="text" value={inputValue} onChange={onInputChange} placeholder="Search..." />
+        <button className="search-button" type="submit">Search</button>
+      </form>
       <Table
         columns={[
           { key: 'title', title: 'Title', dataType: DataType.String },
