7d79f05f2d37981b617f6df6b6aec8a2b2035eb8
Brian White <brian@thirdember.com>
2024-05-03 10:51:55 -0400

    add deploy file

---
+++ deploy_local.zsh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+SERVICENAME="alexandria_web-react"
+
+git fetch
+
+FORCEUPDATE=false
+
+if [[ $1 =~ ^(-f|--force) ]]; then
+    FORCEUPDATE=true
+fi
+
+if $FORCEUPDATE || [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+    DATESTAMP=$(date +%Y-%m-%d)
+    
+    git pull
+    git reset --hard origin/main
+
+    docker build . --tag $SERVICENAME --tag $SERVICENAME:v$DATESTAMP
+    
+    docker-compose down
+    docker-compose up -d
+else
+    echo "$SERVICENAME already up-to-date"
+fi
